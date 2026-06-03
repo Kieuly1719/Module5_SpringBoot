29cc7012e4daf9fa5a8aafd98e755306a2850c04
Kiều Ly <khachhangtl78855@gmail.com>
2026-06-03 11:08:16 +0700

    .

--- Thymeleaf/src/main/java/com/example/thymeleaf/controller/ProductController.java
+++ Thymeleaf/src/main/java/com/example/thymeleaf/controller/ProductController.java
@@ -4,8 +4,7 @@ import com.example.thymeleaf.model.Product;
 import com.example.thymeleaf.service.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class ProductController {
